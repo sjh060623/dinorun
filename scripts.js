1560d68f13588003e8ceda302e25bed61f11344c
sjh060623 <sjh060623@gmail.com>
2022-11-01 15:00:09 +0900

    first commit

---
+++ scripts.js
@@ -0,0 +1,28 @@
+const dino = document.getElementById("dino");
+const cactus = document.getElementById("cactus");
+
+function jump() {
+  if (dino.classList != "jump") {
+    dino.classList.add("jump");
+
+    setTimeout(function () {
+      dino.classList.remove("jump");
+    }, 700);
+  }
+}
+
+let isAlive = setInterval(function () {
+  let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+
+  let cactusLeft = parseInt(
+    window.getComputedStyle(cactus).getPropertyValue("left")
+  );
+
+  if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140) {
+    alert("Game Over!");
+  }
+}, 10);
+
+document.addEventListener("keydown", function (event) {
+  jump();
+});
